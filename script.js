b49724c12208a6239ca57688f540834da3f2d3f7
noeliarodriguez <rodrigueznoeliasabrina@gmail.com>
2017-01-20 17:21:47 -0300

    Primer commit

--- script.js
+++ script.js
@@ -40,4 +40,82 @@ $(document).ready(function () {
 
     });
 
+    //___________________________________________________________
+    //--------------VALIDACION DE FORMULARIO --------------------
+
+    var nombre = $("#name");
+    var email = $("#email");
+    var msj = $("#mensaje");
+
+    function validateUsername(){
+        //NO cumple longitud minima
+        if(nombre.val().length < 3){
+            nombre.addClass("error");
+            $("#name").popover('show');
+            return false;
+        }
+        //SI longitud pero NO solo caracteres A-z
+        else if(!nombre.val().match(/^[a-zA-Z]+$/)){
+            nombre.addClass("error");
+            $("#name").popover('show');
+            return false;
+        }
+        // SI longitud, SI caracteres A-z
+        else{
+            nombre.removeClass("error");
+            $("#name").popover('hide');
+            return true;
+        }
+    }
+
+    function validateEmail(){
+        //NO hay nada escrito
+        if(email.val().length == 0){
+            email.addClass("error");
+            $("#email").popover('show');
+            return false;
+        }
+        // SI escrito, NO VALIDO email
+        else if(!email.val().match(/^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i)){
+            email.addClass("error");
+            $("#email").popover('show');
+            return false;
+        }
+        // SI rellenado, SI email valido
+        else{
+            email.removeClass("error");
+            $("#email").popover('hide');
+            return true;
+        }
+    }
+    function validateMensaje(){
+        if(msj.val()==0){
+            msj.addClass("error");
+            $("#mensaje").popover("show");
+            return false;
+        }
+        else
+        {
+            msj.removeClass("error");
+            $("#mensaje").popover('hide');
+            return true;
+        }
+    }
+
+    //controlamos la validacion en los distintos eventos
+    // Perdida de foco
+        nombre.blur(validateUsername);
+        email.blur(validateEmail);
+
+    // Pulsacion de tecla
+     //   nombre.keyup(validateUsername);
+     //   email.keyup(validateEmail);
+
+    // Envio de formulario
+        $("#form1").submit(function(){
+            return !!(validateUsername() & validateEmail() & validateMensaje());
+        });
+
+
+
 });
